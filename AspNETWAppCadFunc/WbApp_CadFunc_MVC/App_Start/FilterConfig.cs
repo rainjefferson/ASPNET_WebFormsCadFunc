0c944ba5fc13d455000a5bf88ac173568afae7b6
rainjefferson <117676799+rainjefferson@users.noreply.github.com>
2023-11-13 23:16:57 -0300

    Incluindo projeto MVC web forms

---
+++ AspNETWAppCadFunc/WbApp_CadFunc_MVC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace WbApp_CadFunc_MVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ AspNETWAppCadFunc/WbApp_CadFunc_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WbApp_CadFunc_MVC.MvcApplication" Language="C#" %>
